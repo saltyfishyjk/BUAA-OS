aca798218421eaf4aa89137e235a69cb64ca1f25
YJK <20373673@buaa.edu.cn>
2022-03-12 22:41:26 +0800

    test for Exercise0.1

--- src/palindrome.c
+++ src/palindrome.c
@@ -3,18 +3,29 @@ int main()
 {
 	int n;
 	scanf("%d",&n);
+	int num[15] = {0};
+	int ct = 0;
+	int i;
+	while (n != 0) {
+		num[ct++] = n % 10;
+		n /= 10;
+	}
+	
+	int judge = 1;
+
+	for (i = 0 ; i < ct;i++) {
+		if (num[i] != num[ct - i - 1]) {
+			judge = 0;
+			break;
+		}
+	}
+	
 
 
 
 
 
-
-
-
-
-
-
-	if(){
+	if(judge == 1){
 		printf("Y");
 	}else{
 		printf("N");
