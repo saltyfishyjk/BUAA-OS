cc4cfac00b9d4ae9a54fdc3c683b187606a61966
YJK <20373673@buaa.edu.cn>
2022-03-17 15:58:27 +0800

    Try Extra v2.0

--- find_warnings.sh
+++ find_warnings.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 gcc -Wall $1 -o test 2> warning.txt
-
+grep warning:  warning.txt | awk -F warning:\  '{print $1$2}' > result.txt
+if [ $? -eq 0 ]
+then 
+a=1
+while [ $a -ne $2 ]
+do
+a=$[$a+1]
+done
+fi
+pwd >> result.txt
