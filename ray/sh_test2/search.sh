0e4473fd519a2dc7b31b422d96e840b8b67ad022
GitLab <gitlab@git.os.buaa.edu.cn>
2022-03-17 14:13:41 +0800

    Judgement for lab0-exam at 2022-03-17T14:13:41+08:00

--- ray/sh_test2/search.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash
-#First you can use grep (-n) to find the number of lines of string.
-grep -n $2 $1 | awk -F: '{print $1}'> $3
--- src/sh_test/hello_os.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# assignment for lab0
-sed -n '8p' $1 1>$2
-sed -n '32p' $1 1>>$2
-sed -n '128p' $1 1>>$2
-sed -n '512p' $1 1>>$2
-sed -n '1024p' $1 1>>$2
