8c6d389b1a0a72233ecd67b7db5a828a953dee6b
YJK <20373673@buaa.edu.cn>
2022-03-31 15:22:32 +0800

    Try lab1-2-Extra Step3 Debug : try origin version

--- my_cal/my_cal.c
+++ my_cal/my_cal.c
@@ -1,4 +1,4 @@
-char _my_getchar();
+/*char _my_getchar();
 void _my_putchar(char ch);
 inling int isdigit(char c) {
 	return c >= '0' && c <= '9';
@@ -21,4 +21,35 @@ void wr(int x) {
 }
 void my_cal() {
 	wr(rd() + rd());
+}*/
+char _my_getchar();
+void _my_putchar(char ch);
+
+void my_cal() {
+	char c = _my_getchar();
+	int a = 0;
+	int b = 0;
+	while (c != '\n) {
+		a = a * 10 + c - '0';
+		c = _my_getchar();
+	}
+	c = _my_getchar();
+	while (c != '\n') {
+		b = b * 1- + c - '0';
+		c = _my_getchar();
+	}
+	int d = a + b;
+	if (d == 0) { 
+		_my_putchar('0');
+	} else {
+		int e = 1000000000;
+		while (d / e == 0) {
+			e = e / 10;
+		}
+		while (e) { 
+			_my_putchar(d / e + '0');
+			d = d % e;
+			e = e / 10;
+		}
+	}
 }
