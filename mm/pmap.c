a77744e2d13b330c365d71a8acbfe9d53363cb0d
YJK <20373673@buaa.edu.cn>
2022-04-03 00:17:08 +0800

    Try Exercise 2.1

--- mm/pmap.c
+++ mm/pmap.c
@@ -27,9 +27,11 @@ void mips_detect_memory()
 {
 	/* Step 1: Initialize basemem.
 	 * (When use real computer, CMOS tells us how many kilobytes there are). */
-
+	maxpa = 0x4000000;
+	basemem = 0x4000000;
+	extmem = 0x0;
 	// Step 2: Calculate corresponding npage value.
-
+	npage = basemem >> PGSHIFT;
 	printf("Physical memory: %dK available, ", (int)(maxpa / 1024));
 	printf("base = %dK, extended = %dK\n", (int)(basemem / 1024),
 			(int)(extmem / 1024));
