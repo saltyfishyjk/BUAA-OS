c640b7e6ccc715a2aa31c4ef954b9d1f0014ac5d
YJK <20373673@buaa.edu.cn>
2022-04-10 20:05:55 +0800

    Try Exercise 2.5 v1.0

--- mm/pmap.c
+++ mm/pmap.c
@@ -176,18 +176,26 @@ Hint:
 Use `LIST_INSERT_HEAD` to insert something to list.*/
 void page_init(void)
 {
+	struct Page *now;
+	struct Page *last;
 	/* Step 1: Initialize page_free_list. */
 	/* Hint: Use macro `LIST_INIT` defined in include/queue.h. */
-
+	LIST_INIT(&page_free_list);
 
 	/* Step 2: Align `freemem` up to multiple of BY2PG. */
-
+	ROUND(freemem, BY2PG);
 
 	/* Step 3: Mark all memory blow `freemem` as used(set `pp_ref`
 	 * filed to 1) */
-
+	for (now = pages; page2kva(now) < freemem; now++) {
+		now->pp_ref = 1;
+	}
 
 	/* Step 4: Mark the other memory as free. */
+	for (now =  &pages[PPN(PADDR(freemem))]; page2ppn(now) < npage; now++) {
+		now->pp_ref = 0;
+		LIST_INSERT_HEAD(&page_free_list, now, pp_link);
+	}
 }
 
 /* Exercise 2.4 */
@@ -210,12 +218,18 @@ int page_alloc(struct Page **pp)
 	struct Page *ppage_temp;
 
 	/* Step 1: Get a page from free memory. If fail, return the error code.*/
-
+	if (LIST_EMPTY(&page_free_list)) {
+		return -E_NO_MEM;// E_NO_MEM is defined in mm/error.h as 4
+	}
+	ppage_temp = LIST_FIRST(&page_free_list);
+	LIST_REMOVE(ppage_temp, pp_link);
 
 	/* Step 2: Initialize this page.
 	 * Hint: use `bzero`. */
-
-
+	bzero(page2kva(ppage_temp), BY2PG);
+	
+	*pp = ppage_temp;
+	return 0;
 }
 
 /* Exercise 2.5 */
@@ -226,10 +240,15 @@ When you free a page, just insert it to the page_free_list.*/
 void page_free(struct Page *pp)
 {
 	/* Step 1: If there's still virtual address referring to this page, do nothing. */
-
+	if (pp->pp_ref > 0) {
+		return ;
+	}
 
 	/* Step 2: If the `pp_ref` reaches 0, mark this page as free and return. */
-
+	if (pp->pp_ref == 0) {
+		LIST_INSERT_HEAD(&page_free_list, pp, pp_link);
+		return;
+	}
 
 	/* If the value of `pp_ref` is less than 0, some error must occurr before,
 	 * so PANIC !!! */
