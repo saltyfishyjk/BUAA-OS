6225b519beb9b4ac0a408e43e1fb7f2aa889fd2b
YJK <20373673@buaa.edu.cn>
2022-05-02 17:24:50 +0800

    zry debug v2.0 //printf

--- lib/sched.c
+++ lib/sched.c
@@ -17,6 +17,7 @@ void sched_yield(void)
     static int count = 0; // remaining time slices of current env
     static int point = 0; // current env_sched_list index
     static struct Env *e = NULL;
+	//printf("\ncurenv->env_id : %d\n", e->env_id);
     /*  hint:
      *  1. if (count==0), insert `e` into `env_sched_list[1-point]`
      *     using LIST_REMOVE and LIST_INSERT_TAIL.
@@ -54,5 +55,6 @@ void sched_yield(void)
      }
      count--;
      e->env_runs++;
+	// printf("\n%d\n",e->env_id);
      env_run(e);
 }
