5a648cb88632e0e020ecb51faa5b586e5a1b0adc
于敬凯 <20373673@buaa.edu.cn>
2022-05-02 08:29:46 +0000

    zry debug env.c env_alloc

--- lib/env.c
+++ lib/env.c
@@ -258,7 +258,6 @@ env_alloc(struct Env **new, u_int parent_id)
 
     /* Step 5: Remove the new Env from env_free_list. */
 	LIST_REMOVE(e, env_link);
-	LIST_INSERT_HEAD(&env_sched_list[0], e, env_sched_link);
 	*new = e;
 	return 0;
 
