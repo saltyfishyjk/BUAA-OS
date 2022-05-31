eeedf82640c90cd1c7315a57dfc69755c23bb2e4
YJK <20373673@buaa.edu.cn>
2022-06-01 00:31:51 +0800

    Try Exercise 5.1 v1.0

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -484,6 +484,13 @@ int sys_ipc_can_send(int sysno, u_int envid, u_int value, u_int srcva,
 int sys_write_dev(int sysno, u_int va, u_int dev, u_int len)
 {
         // Your code here
+	if (dev >= 0x10000000 && dev + len <= 0x10000020 ||
+		dev >= 0x13000000 && dev + len <= 0x13000420 ||
+		dev >= 0x15000000 && dev + len <= 0x15000200) {
+		bcopy(va, 0xa0000000 + dev, len);
+		return 0;
+	}
+	return -E_INVAL;
 }
 
 /* Overview:
@@ -506,4 +513,11 @@ int sys_write_dev(int sysno, u_int va, u_int dev, u_int len)
 int sys_read_dev(int sysno, u_int va, u_int dev, u_int len)
 {
         // Your code here
+	if (dev >= 0x10000000 && dev + len <= 0x10000020 ||
+		dev >= 0x13000000 && dev + len <= 0x13004200 ||
+		dev >= 0x15000000 && dev + len <= 0x15000020) {
+		bcopy(0XA0000000 + dev, va, len);
+		return 0;
+	}
+	return -E_INVAL;
 }
