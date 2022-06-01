1ccf4cc7a0a49d9eb3fefacc093d9baad2fddafa
YJK <20373673@buaa.edu.cn>
2022-06-01 09:31:54 +0800

    Try Exercise 5.2 v1.0

--- fs/ide.c
+++ fs/ide.c
@@ -30,8 +30,39 @@ ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs)
 	int offset_end = offset_begin + nsecs * 0x200;
 	int offset = 0;
 
+	u_int zero = 0;
+	u_int cur_offset = 0;
+	int op_status = 0;
+	int read = 0;
+	int can_read = 0;
 	while (offset_begin + offset < offset_end) {
 		// Your code here
+		cur_offset = offset_begin + offset; // calc current offset
+		/* set diskno */
+		if (syscall_write_dev(&diskno, 0x13000010, 4) != 0) {
+			user_panic("write failed!\n");
+		}
+		/* set offset */
+		if (syscall_write_dev(&cur_offset, 0x13000000, 4) != 0) {
+			user_panic("write failed!\n");
+		}
+		/* set value */
+		if (syscall_write_dev(&read, 0x13000020, 4) != 0) {
+			user_panic("write failed!\n");
+		}
+
+		if (syscall_read_dev(&op_status, 0x13000030, 4) != 0) {
+			user_panic("write failed!\n");
+		}
+		if (op_status = 0) {
+			user_panic("read failed!\n");
+		}
+		if (syscall_read_dev(dst + offset, 0x13004000, 0x200) != 0) {
+			user_panic("read failed!\n");
+		}
+	
+		offset += 0x200;
+		
 		// error occurred, then panic.
 	}
 }
@@ -55,16 +86,40 @@ void
 ide_write(u_int diskno, u_int secno, void *src, u_int nsecs)
 {
 	// Your code here
-	// int offset_begin = ;
-	// int offset_end = ;
-	// int offset = ;
+	int offset_begin = secno * 0x200;
+	int offset_end = offset_begin + nsecs * 0x200;
+	int offset = 0;
+	int offset_now = 0;
+	int op_status = 0;
+	int write = 1;
+	int can_read = 0;
 
 	// DO NOT DELETE WRITEF !!!
 	writef("diskno: %d\n", diskno);
 
-	// while ( < ) {
+	while (offset_begin + offset < offset_end) {
 		// copy data from source array to disk buffer.
-
+		offset_now = offset_begin + offset;
+		if (syscall_write_dev(&diskno, 0x13000010, 4) != 0) {
+			user_panic("write failed!\n");
+		}
+		if (syscall_write_dev(&offset_now, 0x13000000, 4) != 0) {
+			user_panic("write failed!\n");
+		}
+		if (syscall_write_dev(src + offset, 0x13004000, 0x200) != 0) {
+			user_panic("read failed!\n");
+		}
+		if (syscall_write_dev(&write, 0x13000020, 4) != 0) {
+			user_panic("write failed!\n");
+		}
+		if (syscall_read_dev(&op_status, 0x13000030, 4) != 0) {
+			user_panic("write failed!");
+		}
+		if (op_status == 0) {
+			user_panic("read failed!\n");
+		}
+		
+		offset += 0x200;
 		// if error occur, then panic.
-	// }
+	 }
 }
